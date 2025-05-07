ba0307e8dbdfc27ffef7ca5f1c7c0153f6e977c0
Neha Ghongade <Neha.Ghongade@ibm.com>
2025-05-07 12:20:16 +0530

    making change in appl to use WMQ instaed of SIB based

--- jms/websimplemessage/src/main/java/javaeetutorial/websimplemessage/ReceiverBean.java
+++ jms/websimplemessage/src/main/java/javaeetutorial/websimplemessage/ReceiverBean.java
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2014 Oracle and/or its affiliates. All rights reserved.
- *
- * You may not modify, use, reproduce, or distribute this software except in
- * compliance with  the terms of the License at:
- * https://github.com/javaee/tutorial-examples/LICENSE.txt
- */
 package javaeetutorial.websimplemessage;
 
 import java.util.logging.Level;
@@ -13,8 +6,8 @@ import javax.annotation.Resource;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import javax.inject.Inject;
 import javax.inject.Named;
+import javax.jms.ConnectionFactory;
 import javax.jms.JMSConsumer;
 import javax.jms.JMSContext;
 import javax.jms.JMSRuntimeException;
@@ -25,40 +18,35 @@ import javax.jms.Queue;
 public class ReceiverBean {
 
     static final Logger logger = Logger.getLogger("ReceiverBean");
-    @Inject
-    private JMSContext context;
-    @Resource(lookup = "java:comp/jms/webappQueue")
+
+    @Resource(lookup = "jms/MyWMQConnectionFactory")
+    private ConnectionFactory connectionFactory;
+
+    @Resource(lookup = "jms/MyWMQQueue")
     private Queue queue;
 
-    /**
-     * Creates a new instance of ReceiverBean
-     */
     public ReceiverBean() {
     }
 
-    /*
-     * Within a try-with-resources block, create context.
-     * Create consumer, then receive message, using a timeout.
-     * Create FacesMessage to display on page.
-     */
     public void getMessage() {
-        try {
+        try (JMSContext context = connectionFactory.createContext()) {
             JMSConsumer receiver = context.createConsumer(queue);
             String text = receiver.receiveBody(String.class, 1000);
 
             if (text != null) {
                 FacesMessage facesMessage =
-                        new FacesMessage("Reading message: " + text);
+                    new FacesMessage("Reading message: " + text);
                 FacesContext.getCurrentInstance().addMessage(null, facesMessage);
             } else {
                 FacesMessage facesMessage =
-                        new FacesMessage("No message received after 1 second");
+                    new FacesMessage("No message received after 1 second");
                 FacesContext.getCurrentInstance().addMessage(null, facesMessage);
             }
         } catch (JMSRuntimeException t) {
             logger.log(Level.SEVERE,
-                    "ReceiverBean.getMessage: Exception: {0}",
-                    t.toString());
+                "ReceiverBean.getMessage: Exception: {0}",
+                t.toString());
         }
     }
 }
+
